d40d72bf447f07f73ddc6ddc6a7d673ef747f9af
PanisaJ <s5701012630140@email.kmutnb.ac.th>
2020-02-07 15:01:09 +0700

    get articles from web

---
+++ myapp/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from bs4 import BeautifulSoup
+import requests 
+
+from urllib.parse import urljoin
+
+def index(request):
+    return render(request, 'myapp/index.html')
+
+def get_article(request):
+    url = 'https://www.blognone.com/node'
+    if request.POST.get('num'):
+        n = int(request.POST.get('num')) 
+        data = {}
+        soup = setup_soup(url)
+        count = 0
+        while len(data) < n:
+            remind_number = n - len(data)
+            #if not first time change url
+            if (count > 0) :
+                #set new soup for next page if number of data less than wanted. 
+                soup = setup_soup(url + "?page=" + str(count))
+            data = find_data(soup, url, remind_number, data)
+            count += 1
+        return render(request, 'myapp/index.html', {'data' : data, 'number' : n})
+    else:
+        return render(request, 'myapp/index.html', {'error_message': "You didn't insert input."})
+    
+
+def find_data(soup, url, number, data):
+    keep_start_index = len(data) + 1
+    for title in soup.find_all("h2", {"itemprop": "name"}, limit=number):
+        url_link = urljoin(url, title.find('a').get('href'))
+        data[len(data)+1] = {'url_link' : url_link, 'title' : title.find('a').get('title')}
+
+    for index, tags in enumerate(soup.find_all("span", {"class" : "terms"}, limit=number)):
+        data[index + keep_start_index]["tags"] = [tag.text for tag in tags.find_all('a')]
+    return data
+
+def setup_soup(url):
+    r = requests.get(url)
+    return BeautifulSoup(r.content, "lxml")
